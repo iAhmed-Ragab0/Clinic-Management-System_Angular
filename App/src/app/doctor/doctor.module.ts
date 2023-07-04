175fbbf51528bed9705826675b89a451a5910660
iAhmed-Ragab0 <iAhmedRagab0@gmail.com>
2023-07-05 02:19:52 +0300

    'comment doctor component'

--- App/src/app/doctor/doctor.module.ts
+++ App/src/app/doctor/doctor.module.ts
@@ -6,7 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { DoctorUpdateComponent } from './components/doctor-update/doctor-update.component';
 import { DoctorComponent } from './doctor/doctor.component';
-import { NgxPaginationModule } from 'ngx-pagination';
+// import { NgxPaginationModule } from 'ngx-pagination';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
@@ -21,7 +21,7 @@ import { MatTableModule } from '@angular/material/table';
     RouterModule,
     ReactiveFormsModule,
     DoctorRoutingModule,
-    NgxPaginationModule,
+    // NgxPaginationModule,
     MatTableModule
 
   ],
